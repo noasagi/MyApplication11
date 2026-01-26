e872e4165b0c4cccd33ad53e9d06351dac426364
noasa <noasagi88@gmail.com>
2026-01-26 20:37:49 +0200

    קצת ניקיון נוסף

--- app/src/main/java/com/example/myapplication/BaseActivity.java
+++ app/src/main/java/com/example/myapplication/BaseActivity.java
@@ -1,28 +1,24 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     protected UserHelper userHelper;
-    private boolean isUpButtonEnabled = false;
 
     @Override
     protected void onStart() {
         super.onStart();
+        // זה חשוב להשאיר כדי שיהיה לך גישה לנתוני משתמש בכל דף
         if (userHelper == null) {
             userHelper = new UserHelper(this);
         }
     }
 
+    // --- הגדרות עיצוב (Toolbar) ---
+    // להשאיר את זה, כי דפים עדיין משתמשים בזה כדי להציג כותרת וחץ אחורה
     protected void setupToolbar(Toolbar toolbar) {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -35,129 +31,19 @@ public abstract class BaseActivity extends AppCompatActivity {
         if (showBackButton && getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
-            isUpButtonEnabled = true;
         }
     }
 
-    public void filterMenuItems(Menu menu) {
-        if (userHelper == null) userHelper = new UserHelper(this);
-        boolean isBusiness = userHelper.isBusinessOwner();
-        boolean isGuest = userHelper.isGuest();
-
-        MenuItem itemHome = menu.findItem(R.id.action_home);
-        MenuItem itemLogin = menu.findItem(R.id.action_login);
-        MenuItem itemLogout = menu.findItem(R.id.action_logout);
-        MenuItem itemSetProfile = menu.findItem(R.id.action_set_profile);
-        MenuItem itemMyBusiness = menu.findItem(R.id.action_my_business);
-        MenuItem itemFavorites = menu.findItem(R.id.action_favorites);
-        MenuItem itemAppointments = menu.findItem(R.id.action_appointments);
-
-        // --- חדש: כפתור לניהול יומן/חסימות ---
-        MenuItem itemBlockSlots = menu.findItem(R.id.action_block_slots);
-
-        if (itemHome != null) itemHome.setVisible(true);
-        if (itemLogin != null) itemLogin.setVisible(isGuest);
-        if (itemLogout != null) itemLogout.setVisible(!isGuest);
-        if (itemSetProfile != null) itemSetProfile.setVisible(!isGuest);
-
-        if (itemMyBusiness != null) {
-            itemMyBusiness.setVisible(isBusiness);
-        }
-
-        if (itemAppointments != null) {
-            itemAppointments.setVisible(!isGuest);
-        }
-
-        // --- לוגיקה חדשה: הצגת ניהול יומן רק לבעל עסק ---
-        if (itemBlockSlots != null) {
-            itemBlockSlots.setVisible(isBusiness);
-        }
-
-        if (itemFavorites != null) {
-            itemFavorites.setVisible(userHelper.isClient());
-        }
-    }
-
-    public boolean handleNavigationItemSelection(MenuItem item) {
-        int id = item.getItemId();
-        if (userHelper == null) userHelper = new UserHelper(this);
-        boolean isBusiness = userHelper.isBusinessOwner();
-
-        if (id == R.id.action_home) {
-            if (isBusiness) {
-                startActivity(new Intent(this, BusinessMainActivity.class));
-            } else {
-                startActivity(new Intent(this, ClientMainActivity.class));
-            }
-            return true;
-        }
-
-        else if (id == R.id.action_my_business) {
-            Toast.makeText(this, "פותחת את ניהול העסק שלי", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(this, MyBusinessMainActivity.class));
-            return true;
-        }
-
-        // --- חדש: ניווט לדף חסימת שעות ---
-        else if (id == R.id.action_block_slots) {
-            startActivity(new Intent(this, BusinessBlockSlotsActivity.class));
-            return true;
-        }
-
-
-
-        else if (id == R.id.action_logout) {
-            FirebaseAuth.getInstance().signOut();
-            userHelper.logout();
-            Toast.makeText(this, "התנתקת בהצלחה", Toast.LENGTH_SHORT).show();
-
-            if (!(this instanceof DrawerBaseActivity)) {
-                invalidateOptionsMenu();
-            }
-
-            Intent intent = new Intent(this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            return true;
-        }
-
-        else if (id == R.id.action_login) {
-            startActivity(new Intent(this, LoginActivity.class));
-            return true;
-        }
-
-        else if (id == R.id.action_favorites) {
-            startActivity(new Intent(this, FavoritesActivity.class));
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (this instanceof DrawerBaseActivity) return false;
-        if (isUpButtonEnabled) return false;
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        filterMenuItems(menu);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
+    // --- טיפול בחץ "אחורה" ---
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        if (handleNavigationItemSelection(item)) {
+        // אם לחצו על החץ אחורה בפינה למעלה
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed(); // תחזור אחורה
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // כל שאר הפונקציות של התפריטים (filterMenuItems, handleNavigation...) נמחקו מכאן!
 }
